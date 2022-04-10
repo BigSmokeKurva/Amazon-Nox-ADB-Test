c21110b41d3f04dd786c6e60d09a9d1421d7cf11
vasya <vasya@DESKTOP-VN4SUGE>
2022-04-10 20:29:04 +0300

    Добавьте файлы проекта.

---
+++ NoxManager.cs
@@ -0,0 +1,154 @@
+﻿using AdvancedSharpAdbClient;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ADBTEST
+{
+    class NoxManager
+    {
+        // Путь до nox
+        string noxPath;
+        int[] instancesIndex;
+        public List<DeviceData> devices = new() { };
+
+        // Конструктор класса
+        public NoxManager(string noxPath, int[] instancesIndex)
+        {
+            this.noxPath = noxPath;
+            this.instancesIndex = instancesIndex;
+        }
+        // Включить adb
+        public void StartAdb()
+        {
+            // Убиваем adb если он включен
+            StopAdb();
+            // Создаем процесс
+            var process = new Process();
+            // Создание процесса
+            process.StartInfo = new ProcessStartInfo
+            {
+                FileName = "cmd.exe",
+                Arguments = @$"/C {noxPath}\adb.exe start-server",
+                RedirectStandardOutput = true,
+                UseShellExecute = false,
+                CreateNoWindow = true,
+            };
+            // Включаем adb
+            process.Start();
+            // Ждем завершения
+            process.WaitForExit();
+            // Ждем полного включения
+            Thread.Sleep(4000);
+        }
+        // Выключить adb
+        public void StopAdb()
+        {
+            var process = new Process();
+            // Создание процесса
+            process.StartInfo = new ProcessStartInfo
+            {
+                FileName = "cmd.exe",
+                Arguments = @$"/C {noxPath}\adb.exe kill-server",
+                RedirectStandardOutput = true,
+                UseShellExecute = false,
+                CreateNoWindow = true,
+            };
+            // 10 раз ввести команду
+            for (var i = 0; i < 10; i++)
+            {
+                // Запуск процесса
+                process.Start();
+                // Ждем завершения
+                process.WaitForExit();
+            }
+            Thread.Sleep(4000);
+
+        }
+        // Создать instance
+        public void Create(int count)
+        {
+
+        }
+        // Проверить жив ли instance
+        public void IsLive(int index)
+        {
+
+        }
+        // Удалить instance
+        public void Remove(int index)
+        {
+
+        }
+        // Удалить все instances
+        public void RemoveAll()
+        {
+
+        }
+        // Перезапустить instance
+        public void Restart(int index)
+        {
+
+        }
+        // Подключиться к instance
+        public void ConnectAll()
+        {
+            // Для проверки запустился ли эмулятор
+            var client = new AdvancedAdbClient();
+            client.Connect("127.0.0.1:5037");
+            var deviceCount = client.GetDevices().Count;
+            // Поочередный запуск
+            foreach (var index in instancesIndex)
+            {
+                // Обьявление процесса
+                using var process = new Process();
+                // Создание процесса
+                process.StartInfo = new ProcessStartInfo
+                {
+                    FileName = "cmd.exe",
+                    Arguments = @$"/C {noxPath}\Nox.exe -clone:Nox_{index}",
+                    RedirectStandardOutput = true,
+                    UseShellExecute = false,
+                    CreateNoWindow = true,
+                };
+                // Включаем instance
+                process.Start();
+                // Проверка запустился ли эмулятор
+                while (true)
+                {
+                    Thread.Sleep(2000);
+                    // Если еще не запустился
+                    if (deviceCount == client.GetDevices().Count) { continue; }
+                    deviceCount = client.GetDevices().Count;
+                    break;
+                }
+            }
+            devices = client.GetDevices();
+        }
+        // Отлключить instance
+        public void DisconnectAll()
+        {
+            // Поочередный запуск
+            foreach (var index in instancesIndex)
+            {
+                // Обьявление процесса
+                var process = new Process();
+                // Создание процесса
+                process.StartInfo = new ProcessStartInfo
+                {
+                    FileName = "cmd.exe",
+                    Arguments = @$"/C {noxPath}\Nox.exe -clone:Nox_{index} -quit",
+                    RedirectStandardOutput = true,
+                    UseShellExecute = false,
+                    CreateNoWindow = true,
+                };
+                // Выключаем instance
+                process.Start();
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,186 @@
+﻿using AdvancedSharpAdbClient;
+using AdvancedSharpAdbClient.DeviceCommands;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Threading;
+
+namespace ADBTEST
+{
+    class NoxThread
+    {
+        // Номер потока
+        int numThread;
+        AdvancedAdbClient client;
+        DeviceData device;
+        Random rnd = new();
+        // Котегории товаров
+        string[] categories =
+        {
+            "1Arts & Crafts",
+            "1Automotive",
+            "1Baby",
+            "1Beauty & Personal Care",
+            "1Computers",
+            "1Books",
+            "1Digital Music",
+            "1Electronics",
+            "1Women's Fashion",
+            "1Men's Fashion",
+            "1Girls' Fashion",
+            "1Boys' Fashion",
+            "1Health & Household",
+            "2Home & Kitchen",
+            "2Industrial & Scientific",
+            "2Kindle Store",
+            "2Luggage",
+            "2Movies & Television",
+            "2Music, CDs & Vinyl",
+            "2Pet Supplies",
+            "2Software",
+            "2Sports & Outdoors",
+            "2Tools & Home Improvement",
+            "2Toys & Games",
+            "2Video Games",
+        };
+        // Конструктор класса
+        public NoxThread(int numThread, AdvancedAdbClient client, DeviceData device)
+        {
+            this.numThread = numThread;
+            this.client = client;
+            this.device = device;
+
+        }
+        // Запуск windscribe
+        public void WindscribeStart()
+        {
+            //--------------------------------------------------------------
+            PackageManager manager = new PackageManager(client, device);
+            Console.WriteLine(manager.Packages.Keys);
+            foreach(var i in manager.Packages.Keys)
+            {
+                Console.WriteLine(i);
+            }
+            foreach (var i in manager.Packages.Values)
+            {
+                Console.WriteLine(i);
+            }
+
+            Console.WriteLine(device.Features);
+            Console.WriteLine(device.Model);
+            Console.WriteLine(device.State);
+            Console.WriteLine(device.Product);
+            Console.WriteLine(client.GetFeatureSet(device).Count);
+            //--------------------------------------------------------------
+            // Запускаем windscribe
+            client.StartApp(device, "com.windscribe.vpn");
+            // Включаем
+            client.FindElement(device, @"//*[@resource-id='com.windscribe.vpn:id/on_off_button']", TimeSpan.FromSeconds(30)).Click();
+            // Ждем когда включится
+            while (true)
+            { // com.windscribe.vpn:id/tv_connection_state
+                Thread.Sleep(2000);
+                var connectionState = client.FindElement(device, @"//*[@resource-id='com.windscribe.vpn:id/tv_connection_state']", TimeSpan.FromSeconds(30));
+                if(connectionState.attributes["text"] == "ON")
+                {
+                    client.HomeBtn(device);
+                    Thread.Sleep(1200);
+                    return;
+                }
+            }
+
+        }
+        // Рандомный элемент из массива
+        public string RndStr(string[] arr) { return arr[rnd.Next(0, arr.Length)]; }
+        // Запуск амазона
+        public void Amazon()
+        {
+            Console.WriteLine($"Поток {numThread} стартанул");
+            // Врубаем windscribe
+            WindscribeStart();
+            // Врубаем amazon
+            client.StartApp(device, "com.amazon.mShop.android.shopping");
+            // Заходим в меню
+            client.FindElement(device, @"//*[@content-desc='Menu. Contains your orders, your account, shop by department, programs and features, settings, and customer service Tab 4 of 4']", TimeSpan.FromSeconds(30)).Click();
+            // Открываем котегории
+            client.FindElement(device, @"//*[@text='Shop by Department']", TimeSpan.FromSeconds(30)).Click();
+            // Выбираем категорию
+            var category = RndStr(categories);
+            // Свайп если 1
+            if (category.StartsWith("1"))
+            {
+                // Свайп
+                client.Swipe
+                    (
+                    device,
+                    client.FindElement(device, "//*[@text=\"Arts & Crafts\"]", TimeSpan.FromSeconds(10)),
+                    client.FindElement(device, @"//*[@text='Shop by Department']", TimeSpan.FromSeconds(10)),
+                    1000
+                    );
+            }
+            // Свайп если 2
+            else
+            {
+                // Свайп
+                client.Swipe
+                    (
+                    device,
+                    client.FindElement(device, "//*[@text=\"Boys' Fashion\"]", TimeSpan.FromSeconds(10)),
+                    client.FindElement(device, "//*[@text=\"Arts & Crafts\"]", TimeSpan.FromSeconds(10)),
+                    1000
+                    );
+                // Свайп
+                client.Swipe
+                    (
+                    device,
+                    client.FindElement(device, "//*[@text=\"Industrial & Scientific\"]", TimeSpan.FromSeconds(10)),
+                    client.FindElement(device, "//*[@text=\"Health & Household\"]", TimeSpan.FromSeconds(10)),
+                    1000
+                    );
+            }
+            // Клик на категорию
+            //client.FindElement(device, $"//*[@text=\"{category[1..]}\"]", TimeSpan.FromSeconds(30)).Click();
+            client.FindElement(device, $"//*[@text=\"Arts & Crafts\"]", TimeSpan.FromSeconds(30)).Click();
+            Element first = client.FindElement(device, @"//*[@class='android.webkit.WebView']/*[@class='android.webkit.WebView']/*[@class='android.view.View']/*[@class='android.view.View'][7]/*[@class='android.view.View'][4]/*[@class='android.view.View'][2]", TimeSpan.FromSeconds(30));
+            Element second = client.FindElement(device, @"//*[@class='android.webkit.WebView']/*[@class='android.webkit.WebView']/*[@class='android.view.View']/*[@class='android.view.View'][7]/*[@class='android.view.View'][2]/*[@class='android.view.View'][2]", TimeSpan.FromSeconds(30));
+            client.Swipe(device, first, second, 100);
+            client.FindElement(device, @"//*[@content-desc='See all results']", TimeSpan.FromSeconds(30)).Click();
+        }
+    }
+    class Program
+    {
+        static AdvancedAdbClient client;
+        static NoxManager nox;
+        static void Main()
+        {
+            // Экземпляр класса 
+            Console.WriteLine("Запуск ADB");
+            nox = new NoxManager
+                (
+                    noxPath: @"E:\Nox\bin",
+                    instancesIndex: new int[] { 0 }
+                );
+            // Запускаем adb
+            nox.StartAdb();
+            Console.WriteLine("ADB включен");
+            // Запускаем эмуляторы
+            nox.ConnectAll();
+            Console.WriteLine("Эмуляторы включены");
+            // Подключение к adb
+            client = new AdvancedAdbClient();
+            client.Connect("127.0.0.1:5037");
+            Console.WriteLine("ADB подключен");
+            // Создаем список запущенных потоков
+            var instancesNox = new List<NoxThread> { };
+            for (var num = 0; num < nox.devices.Count; num++)
+            {
+                instancesNox.Add(new NoxThread(numThread: num, client: client, device: nox.devices[num]));
+                new Thread(instancesNox[num].Amazon).Start();
+            }
+            Console.ReadKey();
+            nox.DisconnectAll();
+        }
+    }
+}
